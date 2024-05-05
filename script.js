1110e68707b9ac47340e477131bc4424ea79f7f0
ABEL MENGSTU <94520834+abeldbase@users.noreply.github.com>
2024-05-05 00:46:25 -0700

    HW 1 - Node.js up and running
    Files in the root directory:
    package.json - with information about your application including your github.com repository url
    index.js - with code to launch a node.js web server on port 3000 and respond to requests
    .gitignore - Your file should exclude node_modules and configuration files for your personal working environment. For example, see https://github.com/brendenwest/it122/blob/main/.gitignoreLinks to an external site. 
    Your web server should send different responses for requests to these urls:
    http://localhost:3000Links to an external site. - shows some home-page information
    http://localhost:3000/aboutLinks to an external site. - returns some information about yourself
    Any other path returns a 404 status code and error message
    Note - this assignment does not require physical files for the different server pages
    Use ES6 syntax for any variable definitions
    Use 'import' syntax for including modules into your index.js script
    Push the completed files to your github.com repo for this class, and submit the repo url to Canvas

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Define variables using 'let'
+let firstName = "Abel";
+let lastName = "Mengstu";
+let yearsOfStudy = 2;
+let goal = "aims to master frontend and backend development, leveraging languages like HTML, CSS, JavaScript, and frameworks such as React.js and Django. I prioritize learning responsive design, security practices, and version control tools like Git, while continually updating my skills to adapt to industry advancements. Through project-based learning and honing soft skills, I aspire to build a robust portfolio and thrive in collaborative environments, ready to tackle real-world challenges in web development.";
