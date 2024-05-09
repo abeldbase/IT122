9a4bbd6f0ecc0ae33a709717a9ce6a59e595a754
ABEL MENGSTU <94520834+abeldbase@users.noreply.github.com>
2024-05-08 20:48:15 -0700

    HW 2 - Express yourself
    Let's simplify your server application and add formatted pages, using Express and basic templating.
    
    As part of this assignment, you'll begin creating a data-driven web application that subsequent assignments will extend. 
    
    NOTE - for this and later assignments, your application should use ES6 module import syntax.
    
    First, create a new JS module called data.js
    Your module should contain the following:
    a JavaScript array of at least 5 objects representing the items you want to showcase  (e.g. cars, music albums, plants, etc).. Each item should have at least 4 attributes,. You should also decide which is the key attribute - e.g. car model, plant name, album title
    a getAll method that returns all array data
    a getItem method that returns a single array item according the requested key
    Your module should export the getAll and getItem methods for use in other modules
    Next;
    
    install the express and ejs node modules
    Convert your index.js module to use Express app syntax
    import your data.js module into index.js
    Update your default route to:
    render responses using a home.ejs template
    show the key attribute for each item in your data array
    link each item to a detail route with identifying query parameter 
    Create a new  detail route that:
    renders responses with a detail.ejs template
    shows an appropriate page tile
    shows detail for the requested item if found in your data array
    contains a link back to the home page

--- index.js
+++ index.js
@@ -1,33 +1,165 @@
-const http = require('http');
-const url = require('url');
-const hostname = '127.0.0.1';
-const port = 3000;
+// index.js
 
-const server = http.createServer((req, res) => {
-  const parsedUrl = url.parse(req.url, true);
-  const path = parsedUrl.pathname;
+import express from 'express';
+import { getAll, getItem } from './employeesData.js';
 
-  console.log('Request received for:', path);
-  if (path === "/") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end(
-        "Hello, IT122 world class students, you have effectively set up your server! This is the home page."
-    );
-} else if (path === "/about") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end(
-        "Hello, This is the about page."
-    );
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded()); // Parse URL-encoded bodies
+
+
+// Route for the home page
+app.get('/', (req, res) => {
+  const employees = getAll();
+  res.render('home', { employees });
+});
+
+// Route for the detail page
+app.get('/detail', (req, res) => {
+  const id = req.query.id;
+  console.log(typeof id)
+  const employee = getItem(Number(id));
+  console.log(employee)
+  if (employee) {
+    res.render('detail', { employee });
   } else {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('404 Not Found');
+    res.status(404).send('Employee not found');
   }
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(PORT, () => {
+  console.log(`Express server started and listening on port ${PORT}`);
 });
+
+
+
+
+
+
+// import express from 'express';
+// import { getAll, getItem } from './data.js';
+
+// const app = express();
+// app.set('port', process.env.PORT || 3000);
+// app.use(express.static('public'));
+// app.set('view engine', 'ejs');
+// const socialMediaCompanies =[{name: "Facebook",yearFounded: "2004", id:1},
+// {name: "Twitter",yearFounded: "2006", id:2}
+
+// ]
+
+// // Route for the home page
+// app.get('/', (req, res) => {
+//   console.log(req.url)
+//   res.render('home',{socialMediaCompanies})
+  
+// });
+
+// app.get('/socialMediaCompanies/:id', (req, res) => {
+//   let socialMediaCompanies = socialMediaCompanies.find(socialMediaCompanies => socialMediaCompanies.id ==
+//     req.params.id)
+//     if (socialMediaCompanies){
+//   res.send('info for Social media companies: ${socialMediaCompanies.name});
+//     }
+// });
+// app.get('/about', (req, res) => {
+//   console.log(req.url)
+//   res.send('This social media company going to sell stock sson!')
+  
+// });
+
+// // define 404 handler
+// app.get((req, res) => {
+//   res.status = 404;
+//   res.send('404 Not Found');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+
+// import express from 'express';
+// import { getAll, getItem } from './data.js';
+
+// const app = express();
+// app.set('port', process.env.PORT || 3000);
+// app.use(express.static('public'));
+// app.set('view engine', 'ejs');
+
+// const socialMediaCompanies = [
+//   { name: "Facebook", yearFounded: "2004", id: 1 },
+//   { name: "Twitter", yearFounded: "2006", id: 2 }
+// ];
+
+// // Route for the home page
+// app.get('/', (req, res) => {
+//   console.log(req.url);
+//   res.render('home', { socialMediaCompanies });
+// });
+
+// // Route for getting information about a specific social media company
+// app.get('/socialMediaCompanies/:id', (req, res) => {
+//   const socialMediaCompany = socialMediaCompanies.find(company => company.id === parseInt(req.params.id));
+//   if (socialMediaCompany) {
+//     res.send(`Information for Social media company: ${socialMediaCompany.name}`);
+//   } else {
+//     res.status(404).send('Social media company not found');
+//   }
+// });
+
+// // Route for the about page
+// app.get('/about', (req, res) => {
+//   console.log(req.url);
+//   res.send('This social media company is going to sell stock soon!');
+// });
+
+// // 404 handler
+// app.use((req, res) => {
+//   res.status(404).send('404 Not Found');
+// });
+
+// app.listen(app.get('port'), () => {
+//   console.log('Express started');
+// });
+// import express from 'express';
+// import { getAll, getItem } from './employeesData.js';
+
+// const app = express();
+// app.set('port', process.env.PORT || 3000);
+// app.use(express.static('public'));
+// app.set('view engine', 'ejs');
+
+// // Route for the home page
+// app.get('/', (req, res) => {
+//   console.log(req.url);
+//   res.render('home');
+// });
+
+// // Route for getting information about a specific person
+// app.get('/employees/:name', (req, res) => {
+//   const person = getItem('name', req.params.name);
+//   if (person) {
+//     res.json(person);
+//   } else {
+//     res.status(404).send('Employee not found');
+//   }
+// });
+
+// // Route to get all employees
+// app.get('/employees', (req, res) => {
+//   const employees = getAll();
+//   res.json(employees);
+// });
+
+// // 404 handler
+// app.use((req, res) => {
+//   res.status(404).send('404 Not Found');
+// });
+
+// app.listen(app.get('port'), () => {
+//   console.log('Express server started and listening on port ' + app.get('port'));
+// });
+
 
