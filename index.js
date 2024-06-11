16a4f69a13f3344b3a6ff6e15ec2e0fb2da598e1
ABEL MENGSTU <94520834+abeldbase@users.noreply.github.com>
2024-06-10 18:33:24 -0700

    HW 6 - React Detail UI
    Your React home page looks great. Now it's time to bring your 'details' view into React as well.
    
    Create a new 'detail' component that: 
    
    receives & display data for the selected list item
    displays data in an HTML form with an editable field for each item attribute
    saves data edits to application state (no need to save to DB yet)
    has a Clear button that clears the editing form when clicked
    Update your home-page to trigger the display of a selected item when a list item is clicked
    
     
    
    NOTE - Your new home page should show both the list of items and details of the selected item at the same time.
    
    You can refer to the React form examples here - https://react.dev/reference/react-dom/components/inputLinks to an external site.

--- index.js
+++ index.js
@@ -1,70 +1,112 @@
 'use strict'
 
 import express from 'express';
-import hbs from "hbs"
+import hbs from "hbs";
 import { Car } from "./models/car.js";
 
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
 app.use(express.static('./public')); // allows direct navigation to static files
-app.use(express.urlencoded()); //Parse URL-encoded bodies
-app.use(express.json()); //Used to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(express.json()); // Used to parse JSON bodies
 
 import cors from 'cors';
-app.use('/api', cors()); // set Access-Control-Allow-Origin header for api route
+app.use(cors()); // Enable CORS for all routes
 
-// app.engine('hbs', hbs({defaultLayout: "main.hbs"}));
 app.set("view engine", "hbs");
 
-app.get('/', (req,res) => {
+// Register JSON helper
+hbs.registerHelper('json', function(context) {
+    return JSON.stringify(context);
+});
+
+// Web routes
+app.get('/', (req, res, next) => {
     Car.find({}).lean()
         .then((cars) => {
-            res.render('home', { cars });
+            res.render('home_react', { cars });
         })
         .catch(err => next(err));
 });
 
-app.get('/about', (req,res) => {
+app.get('/about', (req, res) => {
     res.type('text/html');
     res.render('about');
 });
 
-app.get('/detail', (req,res,next) => {
-    Car.findOne({ title:req.query.title }).lean()
+app.get('/detail', (req, res, next) => {   
+    Car.findOne({ _id: req.query.id }).lean()
         .then((car) => {
-            res.render('details', {result: car} );
+            res.render('details', { result: car });
         })
         .catch(err => next(err));
 });
 
-app.post('/detail', (req,res, next) => {
-    Car.findOne({ title:req.body.title }).lean()
+app.post('/detail', (req, res, next) => {
+    Car.findOne({ title: req.body.title }).lean()
         .then((car) => {
-            res.render('details', {result: car} );
+            res.render('details', { result: car });
         })
         .catch(err => next(err));
 });
-app.get('/delete', async (req, res, next) => {
-    try {
-        const result = await Car.deleteOne({ title: req.query.title });
-        let deleted = result.deletedCount !== 0; // deletedCount will be 0 if no docs deleted
-        const total = await Car.countDocuments();
-        res.type('text/html');
-        res.render('delete', { title: req.query.title, deleted: deleted, total: total });
-    } catch (err) {
-        next(err);
-    }
+
+// API routes
+app.get('/api/v1/cars', (req, res, next) => {
+    Car.find({}).lean()
+        .then((cars) => {
+            if (cars) {
+                res.json(cars);
+            } else {
+                return res.status(404).send('Not Found');
+            }
+        })
+        .catch(err => next(err));
 });
 
+app.get('/api/v1/cars/:id', (req, res, next) => {
+    Car.findOne({ _id: req.params.id }).lean()
+        .then((car) => {
+            if (car) {
+                res.json(car);
+            } else {
+                return res.status(404).send('Not Found');
+            }
+        })
+        .catch(err => next(err));
+});
 
+app.post('/api/v1/cars', (req, res, next) => {
+    Car.updateOne({ _id: req.body._id }, req.body, { upsert: true })
+        .then((result) => {
+            res.json({ updated: result.nModified });
+        })
+        .catch(err => next(err));
+});
 
-app.use((req,res) => {
-    res.type('text/plain'); 
+app.delete('/api/v1/cars/:id', (req, res, next) => {
+    Car.deleteOne({ _id: req.params.id })
+        .then((result) => {
+            res.json({ deleted: result.deletedCount });
+        })
+        .catch(err => next(err));
+});
+
+// 404 handler
+app.use((req, res) => {
+    res.type('text/plain');
     res.status(404);
     res.send('404 - Not found');
 });
 
+// 500 handler
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+
 app.listen(app.get('port'), () => {
-    console.log('Express started');    
+    console.log('Express started');
 });
